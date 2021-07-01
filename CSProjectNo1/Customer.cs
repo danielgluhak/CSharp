7574361b70c8447c96753ee7ca6f96461c92805b
MojProfil <gluhakdaniel@gmail.com>
2021-07-01 23:07:34 +0200

    New class

---
+++ CSProjectNo1/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSProjectNo1
+{
+    class Customer
+    {
+        private String firstName;
+        private String lastName;
+        private Boolean isMale;
+
+        
+    }
+}
