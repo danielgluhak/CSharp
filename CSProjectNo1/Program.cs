27aa65e80829e4bae87369616055bdc9f7cb4f90
MojProfil <gluhakdaniel@gmail.com>
2021-07-01 22:30:58 +0200

    Add project files.

---
+++ CSProjectNo1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSProjectNo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Daniel";
+            int age = 27;
+            Console.WriteLine(name);
+        }
+    }
+}
