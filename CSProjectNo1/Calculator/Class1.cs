b50f8893f40c396cb16f6c01afeaf80b30838512
MojProfil <gluhakdaniel@gmail.com>
2021-07-05 22:28:35 +0200

    Methods...

---
+++ CSProjectNo1/Calculator/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSProjectNo1.Calculator
+{
+    class Calculator
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Substract(int x, int y)
+        {
+            return x - y;
+        }
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        public static int Divide(int x, int y)
+        {
+            return x / y;
+        }
+        public static int Percentage(int num, int percentage)
+        {
+            return (num * percentage) / 100;
+        }
+       
+    }
+}
